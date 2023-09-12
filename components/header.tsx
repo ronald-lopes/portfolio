3533cd028db543d7ba03d68aad3574e5d6bd4a3b
Ronald Lopes <rlopes929@gmail.com>
2023-09-12 16:53:51 -0400

    feat: added prettier config

--- components/header.tsx
+++ components/header.tsx
@@ -8,14 +8,14 @@ import { Tabs, Tab } from '@nextui-org/react'
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-center flex-row fixed py-1 px-4 first-letter: w-screen bg-white dark:bg-black dark:bg-opacity-80 bg-opacity-80 backdrop-blur-md">
-      <div className="flex justify-center sm:justify-between w-full max-w-5xl">
-        <a href="/" className="flex gap-2 items-center">
+    <header className="first-letter: fixed flex w-screen flex-row items-center justify-center bg-white bg-opacity-80 px-4 py-1 backdrop-blur-md dark:bg-black dark:bg-opacity-80">
+      <div className="flex w-full max-w-5xl justify-center sm:justify-between">
+        <a href="/" className="flex items-center gap-2">
           <Image src="/icons/logo.svg" width={32} height={32} alt="logo" />
           <p className="font-bold text-inherit">Ronald Lopes</p>
         </a>
 
-        <ul className="hidden sm:flex justify-between items-center">
+        <ul className="hidden items-center justify-between sm:flex">
           <Tabs variant="underlined" size="lg" aria-label="Menu items">
             {siteConfig.navItems.map((item) => (
               <Tab
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <section className="mt-14 flex flex-col items-center justify-center h-[calc(100vh-56px)]">
+    <section className="mt-14 flex h-[calc(100vh-56px)] flex-col items-center justify-center">
       <p className="text-lg font-light text-gray-700 dark:text-gray-300">
         Product Designer and Front-end Developer
       </p>
-      <h1 className="text-6xl font-semibold text-center text-gray-900 dark:text-gray-100 max-w-xl-2">
+      <h1 className="max-w-xl-2 text-center text-6xl font-semibold text-gray-900 dark:text-gray-100">
         I&apos;m passionate about
         <br /> crafting experiences
         <br /> that resonate with users.
