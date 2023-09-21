4773d21ae2eb830428ac96e00d035dbafc5d5095
Ronald Lopes <rlopes929@gmail.com>
2023-09-20 23:48:18 -0400

    feat: improved responsivity

--- components/header.tsx
+++ components/header.tsx
@@ -8,30 +8,45 @@ import { Tabs, Tab } from '@nextui-org/react'
 
 export const Header = () => {
   return (
-    <header className="first-letter: fixed flex w-screen flex-row items-center justify-center bg-white bg-opacity-80 px-4 py-1 backdrop-blur-md dark:bg-black dark:bg-opacity-80">
-      <div className="flex w-full max-w-5xl justify-center sm:justify-between">
+    <header
+      className="
+      fixed z-50 flex w-screen flex-row items-center justify-center 
+      bg-white bg-opacity-80 px-4 py-2 backdrop-blur-md 
+      dark:bg-black dark:bg-opacity-80 md:py-1"
+    >
+      <div className="flex w-full max-w-5xl justify-between">
         <a href="/" className="flex items-center gap-2">
           <Image src="/icons/logo.svg" width={32} height={32} alt="logo" />
           <p className="font-bold text-inherit">Ronald Lopes</p>
         </a>
 
-        <ul className="hidden items-center justify-between sm:flex">
-          <Tabs variant="underlined" size="lg" aria-label="Menu items">
-            {siteConfig.navItems.map((item) => (
-              <Tab
-                key={item.id}
-                className="p-0"
-                title={
-                  <a className="px-2 py-2 dark:text-gray-200" href={item.href}>
-                    {item.label}
-                  </a>
-                }
-              />
-            ))}
-          </Tabs>
-
-          <ThemeSwitch />
-        </ul>
+        <div className="flex">
+          <ul className="hidden items-center justify-between sm:flex">
+            <Tabs
+              aria-label="Menu items"
+              variant="underlined"
+              size="md"
+              classNames={{
+                tabList: 'relative w-full gap-0 rounded-none p-1',
+                cursor: 'w-[50%]',
+                tab: 'p-0',
+                tabContent: 'p-0 hover:text-gray-500 dark:hover:text-gray-100',
+              }}
+            >
+              {siteConfig.navItems.map((item) => (
+                <Tab
+                  key={item.id}
+                  title={
+                    <a className="px-2 py-3 text-[1rem]" href={item.href}>
+                      {item.label}
+                    </a>
+                  }
+                />
+              ))}
+            </Tabs>
+          </ul>
+          <ThemeSwitch className="p-2" />
+        </div>
       </div>
     </header>
   )
