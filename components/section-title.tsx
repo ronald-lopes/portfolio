281e8c30c7f0cb32f1e44d9348fcf6286e2c7fd1
Ronald Lopes <rlopes929@gmail.com>
2023-09-24 17:48:16 -0400

    feat: updated dark theme colors

--- components/section-title.tsx
+++ components/section-title.tsx
@@ -17,7 +17,7 @@ export const Title = (props: TitleProps) => {
       <span
         className="
         sm:text-md bg-gradient-to-r from-red-900 to-violet-900 bg-clip-text 
-        text-sm text-transparent dark:from-red-500 dark:to-violet-500"
+        text-sm text-transparent dark:from-red-400 dark:to-violet-400"
       >
         {props.subtitle}
       </span>
